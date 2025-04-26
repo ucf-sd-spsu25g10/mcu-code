8afc418a1baf85d41939288ff5bf2d9f197d7961
Michael Castiglia <michael@mcastiglia.com>
2025-04-26 12:43:56 -0400

    Changing credits, ensured that it compiles on the unit

--- src/Components.cpp
+++ src/Components.cpp
@@ -1,8 +1,6 @@
-/* Blink Example with Web Server for ESP32
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
+/* NAVIS MCU Code - UCF Senior Design Project - ECE Group 10 - Spring 2025 / Summer 2025
+  Maintained by Michael Castglia with contributions from Pavan Senthil and Aden McKinney.
+  This code is modified from a sample from the PlatformIO Base Library for the ESP32.
 */
 
 #include <Arduino.h>
